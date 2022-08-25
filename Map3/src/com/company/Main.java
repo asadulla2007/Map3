16e62f5a43012005edac623b9b298881429e268f
asadulla2007 <110012942+asadulla2007@users.noreply.github.com>
2022-08-25 02:11:43 -0400

    Add files via upload

---
+++ Map3/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+
+        System.out.print("L");
+        Thread.sleep(500);
+        System.out.print("o");
+        Thread.sleep(500);
+        System.out.print("a");
+        Thread.sleep(500);
+        System.out.print("d");
+        Thread.sleep(500);
+        System.out.print("i");
+        Thread.sleep(500);
+        System.out.print("n");
+        Thread.sleep(500);
+        System.out.print("g");
+        Thread.sleep(500);
+        System.out.print(".");
+        Thread.sleep(500);
+        System.out.print(".");
+        Thread.sleep(500);
+        System.out.print(".");
+        Thread.sleep(500);
+        System.out.print("!");
+        Thread.sleep(500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
